e882709721348e462757671eaf7d2872cda107de
Douglas Ferreira <doug_casetta@hotmail.com>
2025-08-07 11:24:26 -0300

    Correção de eslint e mudanças no Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,14 +3,14 @@
 function Home() {
     return (
         <>
-            <div className="bg-indigo-900 flex justify-center">
+            <div className="bg-blue-800 flex justify-center">
                 <div className='container grid grid-cols-2 text-white'>
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
                         <h2 className='text-5xl font-bold'>
-                            Seja Bem Vinde!
+                            NoWhereRH
                         </h2>
                         <p className='text-xl'>
-                            Expresse aqui seus pensamentos e opniões
+                            De 'nenhum lugar' para o lugar certo.
                         </p>
 
                         <div className="flex justify-around gap-4">
@@ -20,17 +20,15 @@ function Home() {
                         </div>
                     </div>
 
-                    <div className="flex justify-center ">
+                    <div className=" flex justify-center ">
                         <img
-                            src="https://i.imgur.com/fyfri1v.png"
+                            src="https:ik.imagekit.io/zddqh4rhi/Apresenta%C3%A7%C3%A3o%20Gest%C3%A3o%20de%20Recursos%20Humanos%20Ilustrado%20Azul%20e%20Bege.png?updatedAt=1754576450200"
                             alt="Imagem Página Home"
                             className='w-2/3'
                         />
                     </div>
                 </div>
             </div>
-
-            <ListaPostagens />
         </>
     )
 }

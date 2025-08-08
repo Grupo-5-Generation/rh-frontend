a416be6d75730a2a41665e33714b25d15b530e53
giulia <giuliabarreto2@gmail.com>
2025-08-08 17:25:32 -0300

    fazendo estilização

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -7,7 +7,7 @@ function Login() {
         <>
             <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold ">
                 <form className="flex justify-center items-center flex-col w-1/2 gap-4" >
-                    <h2 className="text-slate-900 text-5xl ">Entrar</h2>
+                    <h2 className="text-sky-900 text-5xl ">Entrar</h2>
                     <div className="flex flex-col w-full">
                         <label htmlFor="usuario">Usuário</label>
                         <input
@@ -15,31 +15,32 @@ function Login() {
                             id="usuario"
                             name="usuario"
                             placeholder="Usuario"
-                            className="border-2 border-slate-700 rounded p-2"
+                            className="border-2 border-sky-900 bg-white rounded p-2"
 
                         />
                     </div>
-                    <div className="flex flex-col w-full">
+                    <div className="flex flex-col w-full ">
                         <label htmlFor="senha">Senha</label>
                         <input
                             type="password"
                             id="senha"
                             name="senha"
                             placeholder="Senha"
-                            className="border-2 border-slate-700 rounded p-2"
+                            className="border-2  border-sky-900 bg-white rounded p-2"
 
                         />
                     </div>
                     <button
                         type='submit'
-                        className="rounded bg-indigo-400 flex justify-center
-                                   hover:bg-indigo-900 text-white w-1/2 py-2">
+                        className="rounded bg-orange-300 flex justify-center
+                                    hover:bg-orange-400 text-sky-950 w-1/2 py-2">
                         <Link to='/home' className=''>Entrar</Link>
                     </button>
 
-                    <hr className="border-slate-800 w-full" />
+                    <hr className="border-sky-900 w-full" />
 
-                    <p>
+                    <p className='text-center text-sky-900'>
+            
                         Ainda não tem uma conta?{' '}
                         <Link to='/cadastro' className=''>Cadastre-se</Link>
                     </p>
